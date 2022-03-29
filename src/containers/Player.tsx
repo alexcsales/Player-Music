cbe75e8881ee63637f9232da3e7ad8cdec4513fd
Alex Correa Sales <alexxcorrea917@gmail.com>
2022-03-28 23:44:52 -0300

    added playlist and style items.

--- src/containers/Player.tsx
+++ src/containers/Player.tsx
@@ -1,14 +1,14 @@
 import { headerInfo, tracks } from '../services/api'
 import PlaylistHeader from './components/PlaylistHeader';
+import PlaylistItens from './components/PlaylistItens';
 
 const Player = () => {
   return (
     <div className='player__container'>
       <PlaylistHeader {...headerInfo} />
-      {/*tracks.map(track) => (
-          <PlaylistItens {...track} />
-        ))*/}
-
+      {tracks.map((track) => (
+        <PlaylistItens {...track} key={track.id} />
+      ))}
     </div>
   )
 }
--- src/containers/components/PlaylistItens.tsx
+++ src/containers/components/PlaylistItens.tsx
@@ -1,8 +1,33 @@
-const PlaylistItens = () => {
+import { useState } from 'react';
+import { liked, noliked } from '../../assets/img/buttons'
+
+type TrackProps = {
+  title: string;
+  like: boolean;
+  albumName: string;
+  cover: {
+    default: string;
+  };
+}
+
+const PlaylistItens = ({ title, like, cover, albumName }: TrackProps) => {
+  const [isLiked, setIsLiked] = useState(like);
+
+  const handleLike = () => {
+    setIsLiked(!isLiked)
+  }
 
   return (
-    <div>
-      <h3>PlaylistItens</h3>
+    <div className="musica__container">
+      <div className="musica__conteudo">
+        <img src={cover.default} alt={title} className="musica__cover" />
+        <div className="musica__nome">
+          <h4>{title}</h4>&nbsp;-&nbsp;<h6>{albumName}</h6>
+        </div>
+        <button onClick={() => handleLike()} className="botao__like">
+          <img src={isLiked ? liked : noliked} alt="Like" className="like" />
+        </button>
+      </div>
     </div>
   );
 
